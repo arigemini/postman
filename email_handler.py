06b9ded61c30f999cbc4aab3152a35b68ff785ad
Ari <duke.gemini@gmail.com>
2011-11-18 02:08:50 -0600

    Made email_handler a script

--- email_handler.py
+++ email_handler.py
@@ -7,7 +7,7 @@ class Email:
     def __init__(self, filepath):
         self.filepath = filepath
         
-        self.sender = ""
+        self.from_ = ""
         
         self.subject = ""
         
@@ -26,7 +26,7 @@ class Email:
         parser = Parser()
         msg = parser.parse(infile)
         
-        self.sender = msg['From'].strip()
+        self.from_ = msg['From'].strip()
         
         if msg.has_key('To'):
             self.to_list = list(w.strip() for w in msg['To'].split(','))
@@ -42,4 +42,19 @@ class Email:
         
         infile.close()
 
+if __name__ == "__main__":
+    """
+    usage: python email_handler.py [email_files]
+    """
+    import sys
+    
+    for filename in sys.argv[1:]:
+        print "Email file: ", filename
+        email = Email(filename)
+        print "From: ", email.from_
+        print "To: ", email.to_list
+        print "Cc: ", email.cc_list
+        print "Bcc: ", email.bcc_list
+        print "Subject: ", email.subject
+        print "Message:\n", email.message
         
