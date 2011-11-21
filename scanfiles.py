092307bf66daf17e9dfaa0081ed9b685bb07d0f2
Ari <duke.gemini@gmail.com>
2011-11-20 20:51:01 -0600

    Cleaning up scanning of emails

--- scanfiles.py
+++ scanfiles.py
@@ -1,10 +1,11 @@
 import os
+import sys
 from email_handler import Email
 
 word_map = {}
 from_to_map = {}
 
-#MAX_FILES = 10000
+# MAX_FILES = 10000
 scan_counter = 0
 
 
@@ -31,13 +32,12 @@ def parsefile(filepath):
     
     global from_to_map
              
-    if not (email.sender in from_to_map):
-        from_to_map[email.sender] = set()
-    full_list = email.to_list
-    full_list.extend(email.cc_list)
-    full_list.extend(email.bcc_list)
-    for cur_email in full_list:
-        from_to_map[email.sender].add(cur_email);
+    if not (email.from_ in from_to_map):
+        from_to_map[email.from_] = set()
+    
+    from_to_map[email.from_].update(email.to_list)
+    from_to_map[email.from_].update(email.cc_list)
+    from_to_map[email.from_].update(email.bcc_list)
         
     scan_counter += 1
 
@@ -59,40 +59,45 @@ def scan(path, level):
                 scan(fullpath, level + 1)
         elif level > 1 and files[0] != '.':
             parsefile(fullpath)
-        #if scan_counter >= MAX_FILES:
-        #    break
+        # if scan_counter >= MAX_FILES:
+        #     break
    
    
-def dump_word_map():
-    word_map_file = open("wordmap", 'w')
+def dump_word_map(word_map_csv):
+    word_map_file = open(word_map_csv, 'w')
     global word_map
     for k, v in word_map.items():
-        line = k + " : " + str(v) + "\n"
+        line = k + "," + str(v) + "\n"
         word_map_file.write(line)
     word_map_file.close()
     
     
-def dump_from_to_map():
-    from_to_map_file = open("fromtomap", 'w')
+def dump_from_to_map(from_to_map_csv):
+    from_to_map_file = open(from_to_map_csv, 'w')
     global from_to_map
     for k, v in from_to_map.items():
-        line = k + " :"
+        line = k
         for email in v:
-            line += " " + email
+            line += "," + email
         line += "\n"
         from_to_map_file.write(line)
     from_to_map_file.close()
         
 
 def main():
-    email_path = '/Users/arindam/workplace/CS499/enron_mail_20110402'
+    email_path = '/Users/arindam/workplace/CS499/enron_mail_20110402/maildir'
+    word_map_csv = 'etc/word_map.csv'
+    from_to_map_csv = 'etc/from_to_map.csv'
+
     global scan_counter
     scan(email_path, 0)
     
-    dump_word_map()
-    dump_from_to_map()
-    
-    print "counter: ", scan_counter, " files scanned\n"
+    print >> sys.stderr, "counter: ", scan_counter, " files scanned\n"
+
+    print >> sys.stderr, "Dumping word map to csv file", word_map_csv, "..."
+    dump_word_map(word_map_csv)
+    print >> sys.stderr, "Dumping from_top map to csv file", from_to_map_csv, "..."
+    dump_from_to_map(from_to_map_csv)
 
 
 if __name__ == "__main__":
