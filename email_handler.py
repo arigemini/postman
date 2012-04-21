5ac5b926ed9cccee3fda84824e7b648ce1a6cfd2
Ari <duke.gemini@gmail.com>
2012-04-21 11:55:39 -0500

    Rename remove_original_message to actual_message

--- email_handler.py
+++ email_handler.py
@@ -4,6 +4,13 @@ class Email:
     """ 
     Parses an email given the file.
     """
+    FROM_KEY = "From"
+    SUBJECT_KEY = "Subject"
+    TO_KEY = "To"
+    CC_KEY = "Cc"
+    BCC_KEY = "Bcc"
+    ACTUAL_MESSAGE_DELIMITERS = ['-Original Message-', '-- Forwarded by']
+    
     def __init__(self, filepath):
         self.filepath = filepath
         
@@ -16,7 +23,7 @@ class Email:
         self.bcc_list = []
         
         self.message = ""
-        
+        self._actual_message = None
         self._parseFile()
         
     def _parseFile(self):
@@ -26,7 +33,13 @@ class Email:
         parser = Parser()
         msg = parser.parse(infile)
         
+        if not msg.has_key('From'):
+            raise RuntimeError, "{0} does not have From field".format(self.filepath)
         self.from_ = msg['From'].strip()
+        
+        if not msg.has_key('Subject'):
+            raise RuntimeError, "{0} does not have Subject field".format(self.filepath)
+        self.subject = msg['Subject'].strip()
         
         if msg.has_key('To'):
             self.to_list = list(w.strip() for w in msg['To'].split(','))
@@ -34,22 +47,21 @@ class Email:
             self.cc_list = list(w.strip() for w in msg['Cc'].split(','))
         if msg.has_key('Bcc'):
             self.bcc_list = list(w.strip() for w in msg['Bcc'].split(','))
-        self.subject = msg['Subject'].strip()
+        
         self.message = msg.get_payload().strip()
         
         infile.close()
         
     @property
-    def remove_original_message(self):
-        idx = self.message.find('-----Original Message-----')
-        if idx != -1:
-            return self.message[:idx].strip()
-        else:
-            idx = self.message.find('---------------------- Forwarded by')
+    def actual_message(self):
+        if self._actual_message:
+            return self._actual_message
+        self._actual_message = self.message
+        for delim in Email.ACTUAL_MESSAGE_DELIMITERS:
+            idx = self._actual_message.find(delim)
             if idx != -1:
-                return self.message[:idx].strip()
-            else:
-                return self.message
+                self._actual_message = self._actual_message[:idx].strip('-').strip()
+        return self._actual_message
         
 
 if __name__ == "__main__":
@@ -59,13 +71,13 @@ if __name__ == "__main__":
     import sys
     
     for filename in sys.argv[1:]:
-        print "**Email file: ", filename
+        print "** Email file: ", filename
         email = Email(filename)
-        print "**From: ", email.from_
-        print "**To: ", email.to_list
-        print "**Cc: ", email.cc_list
-        print "**Bcc: ", email.bcc_list
-        print "**Subject: ", email.subject
-        print "**Message:\n", email.message
-        print "**Without original message: \n", email.remove_original_message
+        print "\t** From: ", email.from_
+        print "\t** To: ", email.to_list
+        print "\t** Cc: ", email.cc_list
+        print "\t** Bcc: ", email.bcc_list
+        print "\t** Subject: ", email.subject
+        print "\t** Message:\n", email.message
+        print "\t** Actual message: \n", email.actual_message
         
