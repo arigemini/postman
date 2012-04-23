dd31bdf6a815a7973dbbef997342f0974e1d130b
Ari <duke.gemini@gmail.com>
2012-04-23 00:33:45 -0500

    Also use emails.snow file if provided

--- utils/check_line.py
+++ utils/check_line.py
@@ -1,25 +1,37 @@
 import sys
 
-def main(label_ids_file, feature_ids_file):
-    labels = set()
-    with open(label_ids_file, 'r') as f:
+def main(id_mappings_file, emails_file):
+    id_to_value = {}
+    
+    email_list = []
+    if emails_file:
+        with open(emails_file, 'r') as f:
+            for line in f:
+                email_list.append(line.strip())
+    
+    with open(id_mappings_file, 'r') as f:
         for line in f:
             line = line.strip()
-            if line:
-                labels.add(line)
-    with open(feature_ids_file, 'r') as f:
-        for line in f:
-            if not "to" in line:
-                continue
-            line = line.strip()
-            splits = line.split(" ")
-            label = splits[0].replace(":", "")
-            if label in labels:
-                print label, splits[2]
-                 
+            idx = line.index(':')
+            cur_id, value = line[:idx], line[idx + 2:]
+            if id in id_to_value:
+                raise RuntimeError, "id {0} occurs twice in id_mappings file".format(id)
+            id_to_value[cur_id] = value
+    
+    for line_num, line in enumerate(sys.stdin):
+        print "Line {0} - {1}".format(line_num, line.strip())
+        if email_list:
+            print "  - Email: {0}".format(email_list[line_num])
+        line = line.strip().strip(':')
+        for cur_id in line.split(','):
+            if cur_id not in id_to_value:
+                print "id {0} not found in id_mappings_file".format(cur_id)
+            print "  - {0}: {1}".format(cur_id, id_to_value[cur_id])
+        
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print "usage: python", __file__, "labels.snow feature_id_to_string_mappings.snow"
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
+        print "usage: cat <csv file with feature ids> | python", __file__, "<id_mappings_file> [emails_file]"
         exit(1)
-    main(sys.argv[1], sys.argv[2])
+    emails_file = sys.argv[2] if len(sys.argv) ==3 else None
+    main(sys.argv[1], emails_file)
