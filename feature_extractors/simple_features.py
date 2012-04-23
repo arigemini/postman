d37c54dbce146c09e4bca5bb26f8f650e9e2f40d
Ari <duke.gemini@gmail.com>
2012-04-23 00:33:33 -0500

    Remove whitespace within fields

--- feature_extractors/simple_features.py
+++ feature_extractors/simple_features.py
@@ -3,20 +3,20 @@ class SimpleFeatures(object):
         pass
     
     def features(self, email):
-        features = {'from': email.from_, 'subject': email.subject}
+        features = {'from': email.from_, 'subject': " ".join(email.subject.split())}
         actual_message = email.actual_message
         
         # Thanks Regards Best
         idx = actual_message.find('Thanks,')
         if idx != -1:
-            features['senderInfo'] = actual_message[idx + 7:].strip()
+            features['senderInfo'] = " ".join(actual_message[idx + 7:].split())
         # Hi Hello Dear *, *\n
         idx = actual_message.find('Hi ')
         if idx != -1 and actual_message[idx + 3].isupper():
             end = idx + 3
             while actual_message[end].isalpha():
                 end += 1
-            features['toInfo'] = actual_message[idx + 3: end]
+            features['toInfo'] = " ".join(actual_message[idx + 3: end].split())
         return features
 
 if __name__ == "__main__":
