354d2feddac4c04547b95d3a7efb48d200c9785f
Ari <duke.gemini@gmail.com>
2012-04-23 20:35:11 -0500

    splitting subject field and using only first-3 as features

--- feature_extractors/simple_features.py
+++ feature_extractors/simple_features.py
@@ -1,37 +1,19 @@
 import string
 
 class SimpleFeatures(object):
-    
-    GREETINGS = ["Hi", "Hello", "Dear", "Hey"]
-        
+            
     def __init__(self):
         pass
     
     def features(self, email):
-        features = {'from': email.from_, 'subject': " ".join(email.subject.split())}
-        actual_message = email.actual_message
-        
-        split_message = actual_message.split()
+        features = {}
+        features['from'] = email.from_
+        features['subject'] = filter(lambda x: x not in ["RE", "FW", "Re"], 
+                                        map(lambda x: x.strip(string.punctuation), 
+                                                email.subject.split()))
         features['first-3'] = filter(lambda x: x and x[0].isupper(),
                                         map(lambda x: x.strip(string.punctuation), 
-                                                split_message[:3]))
-        
-        # Try to get the toInfo from an email, by checking if the email starts
-        # with a greeting or just the name of the person
-        toInfo = None
-        if len(split_message) > 1 and actual_message[len(split_message[0])] in "\n,":
-            toInfo = split_message[0].strip(string.punctuation)
-        else:
-            for greeting in SimpleFeatures.GREETINGS:
-                if actual_message.startswith(greeting):
-                    pos = len(greeting) + 1;
-                    while pos < len(actual_message) and actual_message[pos].isalpha():
-                        pos += 1
-                    toInfo = actual_message[len(greeting) + 1:pos]
-                    break
-        if toInfo and toInfo not in ["Thanks", "thanks", "OK", "ok", "FYI"] + SimpleFeatures.GREETINGS:
-            features['toInfo'] = toInfo
-       
+                                                email.actual_message.split()[:3]))
         return features
 
 if __name__ == "__main__":
